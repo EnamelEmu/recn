f919939e59168c199359cfb2e66eb65f2a142c46
em <mm@seed.mm>
2022-04-12 16:54:11 +0200

    further modified dorkgo

--- src/dork/dork.go
+++ src/dork/dork.go
@@ -1,7 +1,9 @@
 package dork
 
 import (
+	"bufio"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"net/http"
 	"os"
@@ -21,14 +23,30 @@ func helper() {
 	fmt.Println("Usage: dork -a [google CSE api key] -c [config file] [string]")
 }
 
-
-func readapiandcx(input gglConfig) {
-	cseapi = input.cseapiConf
-	cxapi = input.cxapiConf
+func readapiandcx(inputJson []byte) {
+	var xgglconfig gglConfig
+	err := json.Unmarshal(inputJson, xgglconfig)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
 }
 
 // exact terms, exclude terms, filetype, linksite, sitesearch
 
 func getCse(query string) () {
 	resp, err := http.Get("https://customsearch.googleapis.com/customsearch/v1?key=" + apikey + "&cx=" + cxid + "&q=" + query)
+}
+
+func main() {
+	flag.Parse()
+	stat, _ := os.Stdin.Stat()
+	if(stat.Mode() & os.ModeCharDevice) != 0 {
+		fmt.Fprintln(os.Stderr, "No stdin input detected")
+		os.Exit(1)
+	}
+	s := bufio.NewScanner(os.Stdin)
+	for s.Scan() {
+		url := s.Text()
+
+	}
 }
