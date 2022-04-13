6b11a2677e377f4d7564d60f7751cf97d43b3b92
em <mm@seed.mm>
2022-04-13 09:44:48 +0200

    As before

--- src/dork/dork.go
+++ src/dork/dork.go
@@ -1,55 +1,74 @@
 package dork
 
 import (
-	"bufio"
+	//"bufio"
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
 )
 
 var (
 	cseapi string
-	cxapi string
+	cxid string
 )
 
 type gglConfig struct {
 	cseapiConf string
-	cxapiConf string
+	cxidConf string
+}
+
+type gglResponse struct {
+
 }
 
 func helper() {
-	fmt.Println("Usage: dork -a [google CSE api key] -c [config file] [string]")
+	fmt.Println("Usage: dork -c [config file] [string]")
 }
 
-func readapiandcx(inputJson []byte) {
-	var xgglconfig gglConfig
-	err := json.Unmarshal(inputJson, xgglconfig)
+func readapiandcx(path string) error {
+
+	dat, err := os.ReadFile(path)
 	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	inputJson := []byte(dat)
+	var xgglconfig gglConfig
+	err2 := json.Unmarshal(inputJson, xgglconfig)
+	if err2 != nil {
 		fmt.Println("Error: ", err)
 	}
 	cseapi = xgglconfig.cseapiConf
-	cxapi = xgglconfig.cxapiConf
+	cxid = xgglconfig.cxidConf
+	return nil
 }
 
 // exact terms, exclude terms, filetype, linksite, sitesearch
 
-func getCse(query string) () {
-	resp, err := http.Get("https://customsearch.googleapis.com/customsearch/v1?key=" + apikey + "&cx=" + cxid + "&q=" + query)
+func getCse(query string) ([]byte, error) {
+	resp, err := http.Get("https://customsearch.googleapis.com/customsearch/v1?key=" + cseapi + "&cx=" + cxid + "&exactTerms=" + query)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return nil, err
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return nil, err
+	}
+	return body, nil
+}
+
+func parseCse(query string) error {
+	fmt.Println("nil")
+	return nil
 }
 
 func main() {
-	var cFlag = flag.String("c", "./creds.json", "cx id and api keys for google custom search engine service")
+	//var cFlag = flag.String("c", "", "cx id and api keys for google custom search engine service")
 	flag.Parse()
-	stat, _ := os.Stdin.Stat()
-	if(stat.Mode() & os.ModeCharDevice) != 0 {
-		fmt.Fprintln(os.Stderr, "No stdin input detected")
-		os.Exit(1)
-	}
-	s := bufio.NewScanner(os.Stdin)
-	for s.Scan() {
-		 := s.Text()
 
-	}
 }
